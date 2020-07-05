fb93e8f23485018cf0865bd7a21a592a3962da5a
raulcordova <racscord@hotmail.com>
2020-07-05 11:03:10 -0500

    Update index.js

--- routes/index.js
+++ routes/index.js
@@ -5,7 +5,7 @@ var mysqlpool = require('../dbconfig');
 /* GET home page. */
 router.get('/', function(req, res, next) {
   mysqlpool.getConnection(function(err, connection) {
-    connection.query('SELECT * FROM mysql_table', function (error, results, fields) {
+    connection.query('SELECT * FROM profile', function (error, results, fields) {
       connection.release();
       if (error) throw error;
       res.render('index', { title: 'Express', data: JSON.stringify(results, null, 4) });
