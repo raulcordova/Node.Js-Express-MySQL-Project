1b21ead67a340208942632366633dfd6a9f79357
raulcordova <racscord@hotmail.com>
2020-07-10 00:55:59 -0500

    Update app.js

--- app.js
+++ app.js
@@ -120,7 +120,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use(favicon(path.join(__dirname, 'public', 'ficon.ico')));
 
 // Linking routes
-app.use('/api/', routes);
+app.use('/', routes);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
